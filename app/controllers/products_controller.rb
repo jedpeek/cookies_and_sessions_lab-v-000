ef526de01ed63015674112eee939bc24150ea523
Jed Peek <jedpeek@yahoo.com>
2018-08-28 15:29:18 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,9 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+    if @product.save
+      current_cart << @product.id
+      
 
   def add
     @product = Product.find(params[:id])
