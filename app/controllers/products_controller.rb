ef3bf3b79b1d8032e0e21540b30adaac57bc42f0
Jed Peek <jedpeek@yahoo.com>
2018-08-28 15:06:48 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,11 +5,9 @@ class ProductsController < ApplicationController
   def add
   @product = Product.find(params[:id])
 
-  # Load the cart from the session, or create a new empty cart.
   cart = session[:cart] || []
   cart << @product.id
 
-  # Save the cart in the session.
   session[:cart] = cart
 end
   end
