0a15a1caf37a180681bcb12b6e93b05d0650f906
Jed Peek <jedpeek@yahoo.com>
2018-08-28 15:11:00 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
 
-  root 'application#add'
+  root 'products#index'
   resources :products
 
   # Example of regular route:
