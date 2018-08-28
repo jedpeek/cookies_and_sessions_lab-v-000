e91b3fdff41996f41cec8272b3533999ff03295b
Jed Peek <jedpeek@yahoo.com>
2018-08-28 15:19:13 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180828145607) do
+ActiveRecord::Schema.define(version: 20180828151824) do
 
   create_table "products", force: :cascade do |t|
-    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "product"
   end
 
 end
